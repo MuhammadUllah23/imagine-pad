37f248911a6190e2b2606236e57e18f741d2d87a
Muhammad <muhammadhullah23@gmail.com>
2021-08-08 01:28:38 -0400

    validate email and username to be present and unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
     has_many :concepts
     has_many :genres, through: :concepts
     has_many :media_types, through: :concepts
+    
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
     has_secure_password
+
 end
