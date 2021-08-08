41d8ea8c368c4d0da7ab84d83f011b0b782be671
Muhammad <muhammadhullah23@gmail.com>
2021-08-08 02:01:32 -0400

    'new' and 'create' method for sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,15 @@
 class SessionsController < ApplicationController 
-    
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:user][:email]) || User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end
+    end
+
 end
