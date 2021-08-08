b4e28ea1a5c3730bac0ed82849a1b0ec1bd46279
Muhammad <muhammadhullah23@gmail.com>
2021-08-08 01:34:22 -0400

    create a new user using '/signup' route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :concepts
   resources :media_types
   resources :genres
+
   resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
