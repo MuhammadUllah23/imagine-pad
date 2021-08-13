49a38b8b42c793270c96bc97508033026cfb1fcd
Muhammad <muhammadhullah23@gmail.com>
2021-08-12 21:10:25 -0400

    Concept Controller. New and Create methods

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -3,5 +3,18 @@ class ConceptsController < ApplicationController
     def index
         @concepts = Concept.all
     end
+
+    def new
+        @concept = Concept.new
+    end
+
+    def create
+        @concept = Concept.new(concept_params)
+        if @concept.save
+            redirect_to concept_path(@concept)
+        else
+            render :new
+        end
+    end
     
 end
