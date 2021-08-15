71402e400a526d0e7e36b09aed9d269480d12bab
Muhammad <muhammadhullah23@gmail.com>
2021-08-14 22:36:15 -0400

    collection selects in creat concept for includes blank; 'choose'

--- app/helpers/concepts_helper.rb
+++ app/helpers/concepts_helper.rb
@@ -1,2 +1,10 @@
 module ConceptsHelper
+
+    def index_header
+        if @genre 
+            content_tag(:h2, "#{@genre.name} Concepts")
+        else
+            content_tag(:h2, "All Concepts")
+        end
+    end
 end
