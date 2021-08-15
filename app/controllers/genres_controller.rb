e4ec397cbaa0f5adc61a65405e17bbadf5711bb1
Muhammad <muhammadhullah23@gmail.com>
2021-08-15 00:15:01 -0400

    new, create, genre_params methods in genre controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,14 @@
 class GenresController < ApplicationController
+    def new
+        @genre = Genre.new
+    end
+
+    def create
+    end
+
+    private
     
+    def genre_params
+        params.require(:genre).permit(:name)
+    end
 end
