03c1f96ac66a9f0b491fee1b6914b3adf1b32790
Muhammad <muhammadhullah23@gmail.com>
2021-08-15 00:08:10 -0400

    Concept accepts nested attributes for genre

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -2,4 +2,11 @@ class Concept < ApplicationRecord
     belongs_to :user
     belongs_to :genre
     belongs_to :media_type
+    accepts_nested_attributes_for :genre #brand_attributes=
+
+    def genre_attributes=(hash_of_attributes)
+        if !hash_of_attributes["name"].blank? 
+            self.genre = Genre.find_or_create_by(hash_of_attributes)
+        end
+    end
 end
